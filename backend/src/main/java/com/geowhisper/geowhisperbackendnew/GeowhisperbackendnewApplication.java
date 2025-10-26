8f1b5606a4d91812171214d725f70eb12dfb80a0
sankirthan-r <sankirthan1811@gmail.com>
2025-10-26 15:13:04 +0530

    refactor: remove unnecessary blank lines in GeowhisperbackendnewApplication.java

--- backend/src/main/java/com/geowhisper/geowhisperbackendnew/GeowhisperbackendnewApplication.java
+++ backend/src/main/java/com/geowhisper/geowhisperbackendnew/GeowhisperbackendnewApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeowhisperbackendnewApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GeowhisperbackendnewApplication.class, args);
 	}
-
 }
