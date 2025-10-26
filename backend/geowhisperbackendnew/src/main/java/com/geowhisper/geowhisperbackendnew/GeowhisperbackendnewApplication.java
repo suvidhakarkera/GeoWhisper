31f7786cc04ab64235b69d80f410e7b2ee815478
Sankirthan <sankirthan1811@gmail.com>
2025-10-26 10:36:19 +0530

    Commit added spring files to backend

---
+++ backend/geowhisperbackendnew/src/main/java/com/geowhisper/geowhisperbackendnew/GeowhisperbackendnewApplication.java
@@ -0,0 +1,13 @@
+package com.geowhisper.geowhisperbackendnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeowhisperbackendnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeowhisperbackendnewApplication.class, args);
+	}
+
+}
---
+++ backend/geowhisperbackendnew/src/test/java/com/geowhisper/geowhisperbackendnew/GeowhisperbackendnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geowhisper.geowhisperbackendnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeowhisperbackendnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
